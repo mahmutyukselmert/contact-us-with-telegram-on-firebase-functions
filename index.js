7f69ad58f8da6648f98b793626b237337f06b3ad
Mahmut Yüksel Mert <mahmutyukselmert@gmail.com>
2025-01-28 22:24:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,67 +10,65 @@
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
-const functions = require("firebase-functions");
+const WEBSITE_NAME = 'mahmutyukselmert.github.io'; //Website name
+const ORIGIN_URL = 'https://mahmutyukselmert.github.io'; //Website URL
 
+const functions = require("firebase-functions");
+const cors = require("cors")({ origin: ORIGIN_URL }); 
 const axios = require('axios');
 
-const TELEGRAM_BOT_TOKEN = 'TELEGRAM_BOT_TOKEN';
-const CHAT_ID = 'CHAT_ID';
-const ALLOWED_ORIGIN = 'YOUR_WEBSITE_URL';
+const TELEGRAM_BOT_TOKEN = ''; //YOUR_BOT_TOKEN
+const CHAT_ID = ''; //YOUR_CHAT_ID
 
 const ipRequestLog = new Map(); 
 
 exports.sendTelegramMessage = functions.https.onRequest(async (req, res) => {
-  try {
-    const { name, email, subject, message } = req.body;
+  cors(req, res, async () => {
+    try {
+      const { name, email, subject, message } = req.body;
 
-    const origin = req.headers.origin || req.headers.referer;
+      if (req.method !== 'POST') {
+        return res.status(405).send({ error: 'Geçersiz istek metodu. Sadece POST destekleniyor.' });
+      }
 
-    if (!origin || !origin.startsWith(ALLOWED_ORIGIN)) {
-      return res.status(403).send({ error: 'Erişim reddedildi: Bu kaynaktan istek yapılamaz.' });
+      if ( !name || !email || !subject || !message) {
+        return res.status(400).send({ error: 'Tüm alanların doldurulması zorunludur!' });
+      }
+
+      const ipAddress = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+
+      const now = Date.now();
+      const lastRequestTime = ipRequestLog.get(ipAddress);
+
+      if (lastRequestTime && now - lastRequestTime < 15 * 60 * 1000) {
+        return res.status(429).send({ error: "Çok fazla istek gönderildi. Lütfen 15 dakika bekleyin." });
+      }
+
+      ipRequestLog.set(ipAddress, now);
+
+      const telegramApiUrl = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
+
+      // Mesaj formatı
+      const formattedMessage = `
+  📬 *${WEBSITE_NAME} İletişim Formundan Yeni Mesaj*
+  ----------------------------------
+  👤 *Ad & Soyad*: ${name}
+  📧 *E-Posta*: ${email}
+  📋 *Konu*: ${subject}
+  ✉️ *Mesaj*: ${message}
+      `;
+
+      const response = await axios.post(telegramApiUrl, {
+        chat_id: CHAT_ID,
+        text: formattedMessage,
+        parse_mode: 'Markdown',
+      });
+
+      return res.status(200).send({ success: true, data: response.data });
+
+    } catch (error) {
+      console.error('Telegram mesajı gönderilemedi:', error);
+      return res.status(500).send({ error: 'Mesaj Gönderilemedi.' });
     }
-
-    if (req.method !== 'POST') {
-      return res.status(405).send({ error: 'Geçersiz istek metodu. Sadece POST destekleniyor.' });
-    }
-
-    if ( !name || !email || !subject || !message) {
-      return res.status(400).send({ error: 'Tüm alanların doldurulması zorunludur!' });
-    }
-
-    const ipAddress = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-
-    const now = Date.now();
-    const lastRequestTime = ipRequestLog.get(ipAddress);
-
-    if (lastRequestTime && now - lastRequestTime < 15 * 60 * 1000) {
-      return res.status(429).send({ error: "Çok fazla istek gönderildi. Lütfen 15 dakika bekleyin." });
-    }
-
-    ipRequestLog.set(ipAddress, now);
-
-    const telegramApiUrl = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
-
-    // Mesaj formatı
-    const formattedMessage = `
-📬 * ${ALLOWED_ORIGIN} İletişim Formundan Yeni Mesaj*
-----------------------------------
-👤 *Ad & Soyad*: ${name}
-📧 *E-Posta*: ${email}
-📋 *Konu*: ${subject}
-✉️ *Mesaj*: ${message}
-    `;
-
-    const response = await axios.post(telegramApiUrl, {
-      chat_id: CHAT_ID,
-      text: formattedMessage,
-      parse_mode: 'Markdown',
-    });
-
-    return res.status(200).send({ success: true, data: response.data });
-
-  } catch (error) {
-    console.error('Telegram mesajı gönderilemedi:', error);
-    return res.status(500).send({ error: 'Mesaj Gönderilemedi.' });
-  }
+  });
 });
